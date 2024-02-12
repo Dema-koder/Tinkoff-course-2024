4bb299310d89f8dda3464ade6995816fcf62002b
Dema-koder <asotruzikovic@gmail.com>
2024-02-12 08:29:08 +0300

    Марафет навел

--- bot/src/main/java/edu/java/bot/Commands/HelpCommand.java
+++ bot/src/main/java/edu/java/bot/Commands/HelpCommand.java
@@ -1,6 +1,7 @@
 package edu.java.bot.Commands;
 
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.Bot;
 
 public class HelpCommand extends CommandHandler{
@@ -11,6 +12,11 @@ public class HelpCommand extends CommandHandler{
 
     @Override
     protected void processRequest(Update update, Bot bot) {
-
+        String response = "/start -- зарегистрировать пользователя\n" +
+            "/help -- вывести окно с командами\n" +
+            "/track -- начать отслеживание ссылки\n" +
+            "/untrack -- прекратить отслеживание ссылки\n" +
+            "/list -- показать список отслеживаемых ссылок";
+        bot.execute(new SendMessage(update.message().chat().id(), response));
     }
 }
--- bot/src/main/java/edu/java/bot/Commands/UntrackCommand.java
+++ bot/src/main/java/edu/java/bot/Commands/UntrackCommand.java
@@ -1,7 +1,10 @@
 package edu.java.bot.Commands;
 
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.Bot;
+import edu.java.bot.Resources.Resource;
+import java.net.MalformedURLException;
 
 public class UntrackCommand extends CommandHandler{
     @Override
@@ -11,6 +14,13 @@ public class UntrackCommand extends CommandHandler{
 
     @Override
     protected void processRequest(Update update, Bot bot) {
-
+        bot.execute(new SendMessage(update.message().chat().id(), "Отправьте ссылку для удаления"));
+        update = bot.getLastUpdate();
+        try {
+            Resource resource = new Resource(update.message().text());
+            bot.execute(new SendMessage(update.message().chat().id(), "Ссылка удалилась"));
+        } catch (MalformedURLException e) {
+            bot.execute(new SendMessage(update.message().chat().id(), "Ссылка некорректна"));
+        }
     }
 }
