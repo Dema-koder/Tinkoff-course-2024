51ce54408a81e8b811d3a763c5da743789c27cde
Dema-koder <asotruzikovic@gmail.com>
2024-03-24 22:28:10 +0300

    partial hw6

---
+++ scrapper/src/main/java/edu/java/domain/jpa/DefaultJpaChatRepository.java
@@ -0,0 +1,43 @@
+package edu.java.domain.jpa;
+
+import edu.java.dto.dbDTO.Chat;
+import edu.java.dto.dbDTO.Link;
+import edu.java.domain.ChatRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+public class DefaultJpaChatRepository implements ChatRepository {
+    private final JpaChatRepository jpaChatRepository;
+
+    @Override
+    @Transactional
+    public void add(long id) {
+        Optional<Chat> chat = findById(id);
+        jpaChatRepository.save(new Chat(id));
+    }
+
+    @Override
+    @Transactional
+    public int remove(long id) {
+        Chat chat = findById(id).get();
+        List<Link> links = chat.getLinks();
+        if (links != null) {
+            links.clear();
+        }
+        jpaChatRepository.deleteNotUsedLinks();
+        return jpaChatRepository.deleteChatById(id);
+    }
+
+    @Override
+    public Optional<Chat> findById(long id) {
+        return jpaChatRepository.findById(id);
+    }
+
+    @Override
+    public List<Chat> findAll() {
+        return jpaChatRepository.findAll();
+    }
+}
--- scrapper/src/main/java/edu/java/dto/dbDTO/Chat.java
+++ scrapper/src/main/java/edu/java/dto/dbDTO/Chat.java
@@ -1,15 +1,36 @@
 package edu.java.dto.dbDTO;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Table(name = "chat")
 public class Chat {
-    private Long id;
-    private Long tgChatId;
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @ManyToMany
+    @JoinTable(
+        name = "chat_link",
+        joinColumns = @JoinColumn(name = "chat_id"),
+        inverseJoinColumns = @JoinColumn(name = "link_id")
+    )
+    private List<Link> links;
+
+    public Chat(long id) {
+        this.id = id;
+    }
 }
